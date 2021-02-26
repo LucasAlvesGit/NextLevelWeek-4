1dc6219eec8dfb1562947ded6780f4da6984dd83
LucasAlvesGIT <lucasalves.developer@gmail.com>
2021-02-26 11:34:57 -0300

    + componente countdown (HTML CSS FINALIZADO)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/CountDown";
+import { Countdown } from "../components/countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
